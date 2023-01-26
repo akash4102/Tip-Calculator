4dcd7643402bef070fc1143e9760ce8e9c00c356
AKASH VERMA <aakashverma4102@gmail.com>
2023-01-26 21:41:03 +0530

    adding css

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//     $("#txt_name").keyup(function() {
+//         alert($(this).val());
+//     });
+// })
+
+var val=$("#txt_name").val();
+
+// $(".tip").click(function(){
+//     alert($(this).attr('id'));
+// })
+
+$("#custom").val();
